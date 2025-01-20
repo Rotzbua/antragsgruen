4fc86b6b3097513ee1488c32d972b5ae62ce1b44
Rotzbua <Rotzbua@users.noreply.github.com>
2025-01-20 02:35:11 +0100

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -11,6 +11,8 @@ $rules  = [
 //    '@PHP81Migration'       => true, // Must be the same as the min PHP version.
 //    '@PHP80Migration:risky' => true,
 'modernize_strpos'                       => true,
+'php_unit_set_up_tear_down_visibility'   => true,
+
 ];
 
 return $config->setRules($rules)->setFinder($finder);
