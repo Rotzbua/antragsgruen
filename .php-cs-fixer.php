726c8ca6552d6d70a5bcc1b394d46f25995d9cca
Rotzbua <Rotzbua@users.noreply.github.com>
2025-01-20 02:35:11 +0100

    fixer
    f

---
+++ .php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+// https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/blob/master/doc/config.rst
+
+$finder = (new PhpCsFixer\Finder())->in(__DIR__);
+
+$config = new PhpCsFixer\Config();
+$config->setRiskyAllowed(true);
+$config->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect()); // Enable multicore.
+
+$rules  = [
+//    '@PHP81Migration'       => true, // Must be the same as the min PHP version.
+//    '@PHP80Migration:risky' => true,
+'modernize_strpos'                       => true,
+];
+
+return $config->setRules($rules)->setFinder($finder);
